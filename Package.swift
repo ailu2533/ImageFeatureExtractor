55c1fc9dc39921dd58acd884b6d49aa0a58dd9e4
ailu <im.ailu@outlook.com>
2025-11-22 16:47:37 +0800

    update

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ailu2533/CustomColor.git", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/ailu2533/CustomColor.git", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "1.2.0"))
 
 //        .package(url: "https://github.com/ailu2533/LemonUtils.git", branch: "main"),
